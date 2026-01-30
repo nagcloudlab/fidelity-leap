dd40672d52afa8eeba0015e429d9e3009d5c2d35
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-01-30 11:35:47 +0530

    exercise

---
+++ play-with-java/src/main/java/com/example/day2/Exercise.java
@@ -0,0 +1,17 @@
+package com.example.day2;
+
+import com.example.day2.model.Dish;
+
+import java.util.List;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+        List<Dish> menu = Dish.getSampleMenu();
+
+        // get low calorie dish names ( < 400 calories ), sorted by calories
+
+    }
+
+}
